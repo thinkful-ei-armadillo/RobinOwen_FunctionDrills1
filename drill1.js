1a00c9aa04cd6bf150355e02a7ab8c0583ea8dec
owencyr <cyroc@dukes.jmu.edu>
2019-01-14 14:19:12 -0500

    added year of birth calculation and message

--- drill1.js
+++ drill1.js
@@ -1,4 +1,5 @@
 
 function createGreeting(name, age) {
-    return `Hi, my name is ${name} and I\'m ${age} years old`
+    const yearOfBirth = 2019 - age;
+    return `Hi, my name is ${name} and I\'m ${age} years old.  I was born in ${yearOfBirth}`
 }
