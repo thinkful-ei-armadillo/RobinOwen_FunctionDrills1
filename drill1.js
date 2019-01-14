9b84a2340629c9855a84abf467c9ab17a708be3c
owencyr <cyroc@dukes.jmu.edu>
2019-01-14 14:42:48 -0500

    check name and age entry

--- drill1.js
+++ drill1.js
@@ -8,6 +8,9 @@ function getYearOfBirth(age){
 }
 
 function createGreeting(name, age) {
+  if (!age || !name){
+    throw new Error('Arguments not valid.');
+  }
   const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old.  I was born in ${yearOfBirth}`;
 }
