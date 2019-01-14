170283f5d20b922beb01c0ff6724045405f582ba
owencyr <cyroc@dukes.jmu.edu>
2019-01-14 14:40:27 -0500

    error handling

--- drill1.js
+++ drill1.js
@@ -1,8 +1,20 @@
 "use strict";
+
 function getYearOfBirth(age){
+  if (age < 0) {
+    throw new Error('Age cannot be negative');
+  }
   return 2019 - age;
 }
+
 function createGreeting(name, age) {
   const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old.  I was born in ${yearOfBirth}`;
+}
+
+try {
+  const greeting1 = createGreeting('derek', -1);
+}
+catch (er) {
+  console.log('Age cannot be negative');
 }
